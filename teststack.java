6d78f6e757656c4b7de49e6898a923b6d1be3c62
prathamj22 <110449212+prathamj22@users.noreply.github.com>
2022-08-16 16:56:20 +0530

    Create teststack.java

---
+++ teststack.java
@@ -0,0 +1,21 @@
+public class Stack {
+	public static void main(String[] args){
+		stack mystack1 = new stack();
+		stack mystack2 = new stack();
+		
+		for (int i=0; i<10; i++)
+			 mystack1.push(i);
+		for (int i=10; i<20; i++)
+			 mystack1.push(i);
+			 
+			 System.out.println("stack in mystack1;");
+			 for (int i=0; i < 10; i++)
+			 	system.out.println(mystack.pop());
+			 	
+		System.out.println("stack in mystack1;");
+		for (int i=10; i < 20; i++)
+			system.out.println(mystack.pop());
+		
+		
+	}
+}
